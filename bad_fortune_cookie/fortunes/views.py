98d118b4e9037ed9438d1e0b034f4a2a3c7ba35a
J Kaufman <jkaufman@Js-MacBook-Pro.local>
2018-12-09 16:36:17 -0500

    Adds serializers and views for user, group, fortune

--- bad_fortune_cookie/fortunes/views.py
+++ bad_fortune_cookie/fortunes/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows user groups to be viewed or edited
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+class FortuneViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows you to interact with fortunes
+    """
+    queryset = Fortune.objects.all()
+    serializer_class = FortuneSerializer 
